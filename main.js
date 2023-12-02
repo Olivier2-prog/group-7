e1f5ebc519e10879b9037f00143868481d8cdb2b
Olivier@2 <cyuzuzokwizeraolivier2@gmail.com>
2023-12-02 16:03:04 +0200

    window click added in js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let link=document.getElementById('nav-links');
+
+window.addEventListener('click',function(){
+    link.style.left="-200px";
+},true);
 function showMenu(){
     link.style.left="0";
    
